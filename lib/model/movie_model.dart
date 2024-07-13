705bf2159f57269b6db0932c0679a642344243f5
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-13 20:36:20 +0700

    create movie model

---
+++ lib/model/movie_model.dart
@@ -0,0 +1,80 @@
+/*
+ * *
+ *  * movie_model.dart - MoMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 07/13/2024, 20:33
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 07/13/2024, 20:33
+ *
+ */
+
+class MovieModel {
+  final bool? adult;
+  final String? backdropPath;
+  final List<int>? genreIds;
+  final int? id;
+  final String? originalLanguage;
+  final String? originalTitle;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  final String? releaseDate;
+  final String? title;
+  final bool? video;
+  final double? voteAverage;
+  final int? voteCount;
+
+  MovieModel({
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.id,
+    this.originalLanguage,
+    this.originalTitle,
+    this.overview,
+    this.popularity,
+    this.posterPath,
+    this.releaseDate,
+    this.title,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
+  });
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) => MovieModel(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        genreIds: json["genre_ids"] == null
+            ? null
+            : List<int>.from(json["genre_ids"]!.map((x) => x)),
+        id: json["id"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"]?.toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: json["release_date"],
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"]?.toDouble(),
+        voteCount: json["vote_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "genre_ids": genreIds == null
+            ? null
+            : List<dynamic>.from(genreIds!.map((x) => x)),
+        "id": id,
+        "original_language": originalLanguage,
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date": releaseDate,
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+}
