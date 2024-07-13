81c962f7951137263a4ffea23092b03d860f3df8
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-13 18:17:57 +0700

    create splash

---
+++ lib/page/splash_page.dart
@@ -0,0 +1,30 @@
+/*
+ * *
+ *  * splash_page.dart - MoMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 07/13/2024, 18:13
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 07/13/2024, 18:13
+ *
+ */
+
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
