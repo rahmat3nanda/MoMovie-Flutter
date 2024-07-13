d9893c8135f117e0106eef373c2a2ae16d4cbbfc
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-07-13 18:20:02 +0700

    create api

---
+++ lib/data/API.dart
@@ -0,0 +1,16 @@
+/*
+ * *
+ *  * API.dart - MoMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 07/13/2024, 18:19
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 07/13/2024, 14:52
+ *
+ */
+
+class API {
+  var genre = _APIGenre();
+}
+
+class _APIGenre {
+  final String movie = "genre/movie/list";
+}
